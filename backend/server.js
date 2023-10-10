7992a7f4e9a28b1e2d7a1117d5f920b962e2470a
marie-maina <marie.maina@student.studentmoringa.com>
2023-10-10 13:03:06 +0300

    added db

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb+srv://mariemaina:user1234@cluster0.kgghcac.mongodb.net/';
-let dbName = 'Cluster0';
+let mongodb_url = 'mongodb+srv://mariemaina:user1234@yolomy.csbx2tw.mongodb.net/';
+let dbName = 'yolomy';
 
 // define a url to connect to the database
 const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
